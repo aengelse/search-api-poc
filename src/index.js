6e3240005f1436cc99ce85702efd5b2f5cc24f5a
Paul van Dam <p.vandam@coolblue.nl>
2017-06-02 11:27:10 +0200

    Add all css from ecommerce

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './style.css';
+import './css/base.critical.css';
+import './css/search.critical.css';
+import './css/base.css';
+import './css/search.css';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
